0d5d48664a75ae4f285917170441a36db4b0f9d8
Maksymus_cp <SND\Maksymus_cp>
2012-10-03 18:52:49 +0000

    latest version

---
+++ Modelling/Modelling/resource.h
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by app.rc
+=======
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by app.rc
+>>>>>>> 5ee04f3a035fe2c3e6c7e7679da55c0ea9c11643
