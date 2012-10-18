8b42890bdd6827a3da03ee6b2696b90ae9234712
Maksymus_cp <SND\Maksymus_cp>
2012-10-18 17:38:24 +0000

    database

---
+++ AIS/AIS/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'aISDataSet.Прац_каф' table. You can move, or remove it, as needed.
+            this.прац_кафTableAdapter.Fill(this.aISDataSet.Прац_каф);
+
+        }
+    }
+}
